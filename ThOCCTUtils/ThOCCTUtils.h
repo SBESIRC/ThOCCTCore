c66cbb7c62aca462bc05821456394ac464636026
董士崇 <dongshichong@thape.com.cn>
2022-12-20 11:17:11 +0800

    Use ThOCCT.props and introduce OCCUtils from submodule

---
+++ ThOCCTUtils/ThOCCTUtils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#if defined(THOCCTUTILS_EXPORTS)
+#   define THOCCTAPI   __declspec(dllexport)
+#else
+#   define THOCCTAPI   __declspec(dllimport)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    // Declare C functions here
+
+
+#ifdef __cplusplus
+}
+#endif
+
